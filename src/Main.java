882a8ea6c677050626aabc3d29a3c889fed729e6
sk0v0roda <steponist@mail.ru>
2023-01-10 20:17:12 +0300

    Написан код для Main, программа на минимальный балл готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-
+        FileMerger searcher = new FileMerger();
+        File file = new File(searcher.getDirectory());
+        searcher.searchAndAddAllFiles(file);
+        searcher.sort();
+        searcher.mergeFiles();
+        System.out.println(String.format("Файлов найдено : %d, мы объединили их согласно условию и " +
+                "записали в %s", searcher.getFilesList().size(),searcher.getResultPath()));
     }
 }
